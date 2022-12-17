bc99a0f6896543708cb2e4b02d8667eb1b22be10
sorayeon <jihwan77@gmail.com>
2022-12-17 23:08:39 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`
    `QuerydslBinderCustomizer` 를 이용하여 간단하게 개발

--- src/main/java/com/takealook/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/takealook/projectboard/repository/ArticleRepository.java
@@ -1,9 +1,28 @@
 package com.takealook.projectboard.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import com.takealook.projectboard.domain.Article;
+import com.takealook.projectboard.domain.QArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%${v}%' or StringExpression::likeIgnoreCase => like '${v}' (%를 직접 넣어줘야함)
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%${v}%' or StringExpression::likeIgnoreCase => like '${v}' (%를 직접 넣어줘야함)
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase); // like '%${v}%' or StringExpression::likeIgnoreCase => like '${v}' (%를 직접 넣어줘야함)
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // like '%${v}%' or StringExpression::likeIgnoreCase => like '${v}' (%를 직접 넣어줘야함)
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase); // like '%${v}%' or StringExpression::likeIgnoreCase => like '${v}' (%를 직접 넣어줘야함)
+    }
 }
