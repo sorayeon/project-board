8195d69e5f1285acea83982e9f2f61467b55434e
sorayeon <jihwan77@gmail.com>
2023-10-08 01:11:05 +0900

    #14 - - 서비스 로직 테스트 코드 업데이트 및 DTO 재설계, 쿼리 메소드 작성
    강의에서 보여주지 않은 나머지 로직의 테스트 코드를 작성하고
    연관된 DTO 를 재설계함
    테스트가 필요로 하는 repository 쿼리 메소드도 작성

--- src/main/java/com/takealook/projectboard/service/ArticleService.java
+++ src/main/java/com/takealook/projectboard/service/ArticleService.java
@@ -2,10 +2,11 @@ package com.takealook.projectboard.service;
 
 import com.takealook.projectboard.domain.type.SearchType;
 import com.takealook.projectboard.dto.ArticleDto;
-import com.takealook.projectboard.dto.ArticleUpdateDto;
+import com.takealook.projectboard.dto.ArticleWithCommentsDto;
 import com.takealook.projectboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,19 +18,19 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     @Transactional(readOnly = true)
-    public Page<ArticleDto> searchArticles(SearchType title, String search_word) {
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
         return Page.empty();
     }
 
     @Transactional(readOnly = true)
-    public ArticleDto searchArticle(long articleId) {
+    public ArticleWithCommentsDto getArticle(Long articleId) {
         return null;
     }
 
     public void saveArticle(ArticleDto dto) {
     }
 
-    public void updateArticle(long articleId, ArticleUpdateDto dto) {
+    public void updateArticle(ArticleDto dto) {
     }
 
     public void deleteArticle(long articleId) {
