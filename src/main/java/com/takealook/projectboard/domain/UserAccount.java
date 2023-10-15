210f18b8385ccd050a292fd2cc3a55ffd3da7223
sorayeon <jihwan77@gmail.com>
2023-10-16 00:47:41 +0900

    #21 - 도메인 코드 설계 정정
    도메인 코드 설계가 일부 잘못되어 있는 것을 발견,
    UserAccount 회원 계정의 userId 는 회원 id 이므로 유니크 해야 하는데,
    해당 속성이 빠져있었다.
    erd 문서에는 email, user_id 의 유니크 키가 표현되지 않음
    바로 잡기로 한다.

--- src/main/java/com/takealook/projectboard/domain/UserAccount.java
+++ src/main/java/com/takealook/projectboard/domain/UserAccount.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(indexes = {
-        @Index(columnList = "userId"),
+        @Index(columnList = "userId", unique = true),
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
